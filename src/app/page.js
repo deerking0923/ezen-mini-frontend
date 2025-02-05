e9d6f234a1eae3541ce81776b449db75482d6091
realdeer <i3295h@naver.com>
2025-02-05 22:49:44 +0900

    슬라이드 채널 추가

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useEffect } from "react";
+import Link from "next/link";
 import "./page.css";
 
 export default function Home() {
@@ -35,7 +36,7 @@ export default function Home() {
         <div className="card card-left">
           <img src="/img1.jpg" alt="추억 사진 1" />
           <div className="card-text">
-            "인간이 더는 생각할 수 없다면 그것은 인간이 아니다. 인간은 점차 품위를 상실하고 있다. 빠르게 선택하는 것이 효율적이라는 것은 이 시대를 대표하는 인간성 오류다."
+          &quot;인간이 더는 생각할 수 없다면 그것은 인간이 아니다. 인간은 점차 품위를 상실하고 있다. 빠르게 선택하는 것이 효율적이라는 것은 이 시대를 대표하는 인간성 오류다.&quot;
             <br />- 프리드리히 니체
           </div>
         </div>
@@ -44,7 +45,7 @@ export default function Home() {
         <div className="card card-right">
           <img src="/img2.jpg" alt="추억 사진 2" />
           <div className="card-text">
-            "진리를 찾는데 있어서 가장 방해가 되는 것은 빈약한 지성이나 거짓된 가상이 아니다. 그것은 바로 선입견과 편견이다."
+          &quot;진리를 찾는데 있어서 가장 방해가 되는 것은 빈약한 지성이나 거짓된 가상이 아니다. 그것은 바로 선입견과 편견이다.&quot;
             <br />- 아르투어 쇼펜하우어
           </div>
         </div>
@@ -53,7 +54,7 @@ export default function Home() {
         <div className="card card-left">
           <img src="/img3.jpg" alt="추억 사진 3" />
           <div className="card-text">
-            "사람이 어떤 일을 하는지도 중요하지만, 사실은 그 일을 하는 사람이 어떤 사람인지가 더 중요하다."
+          &quot;사람이 어떤 일을 하는지도 중요하지만, 사실은 그 일을 하는 사람이 어떤 사람인지가 더 중요하다.&quot;
             <br />- 존 스튜어트 밀
           </div>
         </div>
@@ -62,7 +63,7 @@ export default function Home() {
         <div className="card card-right">
           <img src="/img4.jpg" alt="추억 사진 4" />
           <div className="card-text">
-            "무지를 아는 것이 진정한 앎의 시작이다."
+          &quot;무지를 아는 것이 진정한 앎의 시작이다.&quot;
             <br />- 소크라테스
           </div>
         </div>
@@ -71,14 +72,14 @@ export default function Home() {
         <div className="card card-left">
           <img src="/img5.jpg" alt="추억 사진 5" />
           <div className="card-text">
-            "현재를 지배하는 자는 과거를 지배한다."
+          &quot;현재를 지배하는 자는 과거를 지배한다.&quot;
             <br />- 조지 오웰 &lt;1984&gt;
           </div>
         </div>
-
+        <div>
         {/* 시작 버튼 */}
-        <div className="start-button">
-          <a href="/questions">시작</a>
+          <Link href="/questions">시작</Link>
+          
         </div>
       </section>
     </>
