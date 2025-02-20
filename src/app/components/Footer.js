bd77c1e9ace87d19b4f227661f73c8eddbc55f17
realdeer <i3295h@naver.com>
2025-02-20 23:25:48 +0900

    푸터에 자료 출처 연결하기.

--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -1,6 +1,6 @@
 'use client';
 import React from 'react';
-// import Link from 'next/link';
+import Link from 'next/link';
 import './Footer.css';
 
 export default function Footer() {
@@ -9,9 +9,10 @@ export default function Footer() {
       <div className="footer-content">
         <span>© 2025 스카이 한국 팬사이트</span>
         <nav className="footer-nav">
-          {/* <Link href="/">About</Link>
-          <Link href="/">Contact</Link>
-          <Link href="/">Privacy Policy</Link> */}
+          자료 출처
+          <Link href="https://cafe.naver.com/blacknbiqa/478984">햇비님의 광채 시즌 가이드 보러가기</Link>
+          <Link href="https://sky-children-of-the-light.fandom.com/wiki/Sky:_Children_of_the_Light_Wiki">아이콘은 스카이 위키에서!</Link>
+          {/*<Link href="/">Privacy Policy</Link> */}
         </nav>
       </div>
     </footer>
